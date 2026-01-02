62c191a7a453cafc2a3eebd3a7d943c5248c9126
majam <majamroczek.pl@gmail.com>
2026-01-02 11:06:25 +0100

    chore: clear homepage and create sidebar

---
+++ app/homepage/page.tsx
@@ -0,0 +1,9 @@
+import SideBar from "./sidebar";
+
+export default function HomePage(){
+    return (
+        <div className="w-screen h-screen">
+            <SideBar/>
+        </div>
+    );
+}
---
+++ app/homepage/sidebar.tsx
@@ -0,0 +1,7 @@
+export default function SideBar(){
+    return (
+        <div className="w-1/4 h-full bg-[#1f1f1f]">
+            
+        </div>
+    );
+}
