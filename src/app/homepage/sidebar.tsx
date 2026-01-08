b8e7a405e05856adf180d4d677e69b30bbcbe7b9
majam <majamroczek.pl@gmail.com>
2026-01-08 13:30:51 +0100

    feat: create plates list

--- src/app/homepage/sidebar.tsx
+++ src/app/homepage/sidebar.tsx
@@ -1,11 +1,16 @@
 import Browser from "../homepage/browser";
 import Filters from "../homepage/filters";
 
-export default function SideBar() {
+export interface FiltersProps {
+  selected: string | null;
+  onSelect: (val: string) => void;
+}
+
+export default function SideBar({selected, onSelect} :FiltersProps) {
   return (
     <div className="h-full w-1/4 bg-[#1f1f1f] overflow-y-auto command-scroll">
       <Browser />
-      <Filters/>
+      <Filters selected={selected} onSelect={onSelect}/>
     </div>
   );
 }
