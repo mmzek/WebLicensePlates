721b2973a877e52e0ecc6ef6c88717baa0a753c2
majam <majamroczek.pl@gmail.com>
2026-01-10 02:49:07 +0100

    feat: add district questions to quiz

--- src/hooks/use-quiz-questions.ts
+++ src/hooks/use-quiz-questions.ts
@@ -6,5 +6,11 @@ export function useQuizQuestions() {
   return useQuery<QuizQuestions[]>({
     queryKey: ["quiz"],
     queryFn: getQuizQuestions,
+    staleTime: 1000 * 60 * 10, // keep data fresh for 10 minutes
+    gcTime: 1000 * 60 * 30,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+    retry: 1,
   });
 }
