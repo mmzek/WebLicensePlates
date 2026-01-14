7a7396cce8595a88e05c172503ecd0a729a95320
majam <majamroczek.pl@gmail.com>
2026-01-14 17:47:46 +0100

    fix: add detail hook

--- src/hooks/use-license-plate.ts
+++ src/hooks/use-license-plate.ts
@@ -1,9 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getLicensePlatesByCode } from "../actions/license-plate";
-import type { LicensePlate } from "../actions/license-plate";
+import type { LicensePlates } from "../actions/license-plate";
 
 export function useLicensePlates(code: string | null) {
-  return useQuery<LicensePlate[]>({
+  return useQuery<LicensePlates[]>({
     queryKey: ["license-plates", code],
     queryFn: () => getLicensePlatesByCode(code!),
     enabled: !!code,
